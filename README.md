a4d4012fbc35a917e325e91a51e6e83e8f325b81
derloris <33088743+derloris@users.noreply.github.com>
2017-11-17 00:32:33 +0100

    Update

--- README.md
+++ README.md
@@ -1,26 +1,29 @@
-# geoweb2017-g03-aufgabe3
+# OpenLayers Workshop
 
-------- DEUTSCH / GERMAN ------- 
+Welcome to the **OpenLayers Workshop**. This workshop is designed to give you a comprehensive overview of OpenLayers as a web mapping solution.
 
-ERSTELLER:
+## Setup
 
-Projekt erstellt von maxcazador, derloris, paulkae und balazsnem
+These instructions assume that you are starting with an `openlayers-workshop-en.zip` archive from the latest [workshop release](https://github.com/openlayers/workshop/releases).  In addition, you'll need [Node](https://nodejs.org/) v6 or higher installed to run a development server for the workshop.
 
-ANLEITUNG
+After extracting the zip, change into the `openlayers-workshop-en` directory and install some additional dependencies:
 
-1. Git klonen
-2. Falls nicht vorhanden node.js installieren
-3. Via Kommandozeile den Befehl npm-install eingeben und das Programm installieren
-4. Das Programm via npm-start starten
-5. Im Browser via http://localhost:3000 aufrufen
+    npm install
 
-QUELLEN:
+Now you're ready to start the workshop development server.  This serves up the [workshop documentation]({{book.workshopUrl}}/doc/) in addition to providing a module bundler for the OpenLayers library.
 
-Verwendet Daten von 
+    npm start
 
-OpenLayers https://openlayers.org/ (via https://github.com/openlayers/workshop/releases/tag/v4.3.1.en.3 ),
-wo wiederum OpenStreetMap (https://openstreetmap.org) verwendet wird.
+This will start a development server where you can read the workshop documentation and work through the exercises.  You should be able to confirm that things are working by seeing an alert pop up at {{book.workshopUrl}}/.  You can read through the workshop documentation at {{book.workshopUrl}}/doc/.
 
-WEITERE KOMMENTARE:
+## Overview
 
-Viel Spaß noch!
+This workshop is presented as a set of modules.  In each module you will perform tasks designed to achieve a specific goal for that module.  Each module builds upon lessons learned in previous modules and is designed to iteratively build up your knowledge base.
+
+The following modules will be covered in this workshop:
+
+* [Basics](basics/README.md) - Learn how to add a map to a webpage.
+* [Vector Data](vector/README.md) - Working with vector data.
+* [Vector Tiles](vectortile/README.md) - Create beautiful maps with vector tiles.
+* [Raster Operations](raster/README.md) - Manipulating pixels with a raster source.
+* [Deploying](deploying/README.md) - Building applications for production.
--- main.js
+++ main.js
@@ -1,46 +1,105 @@
 import 'ol/ol.css';
+import 'javascript-autocomplete/auto-complete.css';
 import Map from 'ol/map';
 import View from 'ol/view';
 import TileLayer from 'ol/layer/tile';
-import Stamen from 'ol/source/stamen';
-import VectorLayer from 'ol/layer/vector';
-import Vector from 'ol/source/vector';
-import GeoJSON from 'ol/format/geojson';
-import Style from 'ol/style/style';
-import Text from 'ol/style/text';
-import Stroke from 'ol/style/stroke';
+import XYZSource from 'ol/source/xyz';
+//! [import-proj]
 import proj from 'ol/proj';
+//! [import-proj]
+import VectorLayer from 'ol/layer/vector';
+import VectorSource from 'ol/source/vector';
+import Feature from 'ol/feature';
+import Point from 'ol/geom/point';
+import Style from 'ol/style/style';
+import IconStyle from 'ol/style/icon';
+import Overlay from 'ol/overlay';
+//! Suchzeile
+import GeoJSON from 'ol/format/geojson';
+import {apply} from 'ol-mapbox-style';
+import AutoComplete from 'javascript-autocomplete';
 
+//! [map-const]
 const map = new Map({
+
+//! [map-const]
   target: 'map',
-  view: new View({
-    center: proj.fromLonLat([16.37, 48.2]),
-    zoom: 13
-  })
-});
-map.addLayer(new TileLayer({
-  source: new Stamen({
-    layer: 'watercolor'
-  })
-}));
-
-const layer = new VectorLayer({
-  source: new Vector({
-    url: 'data/map.geojson',
-    format: new GeoJSON()
-  })
-});
-map.addLayer(layer);
-
-layer.setStyle(function(feature) {
-  return new Style({
-    text: new Text({
-      text: feature.get('name'),
-      font: 'Bold 14pt Verdana',
-      stroke: new Stroke({
-        color: 'white',
-        width: 3
+  layers: [
+    new TileLayer({
+      source: new XYZSource({
+        url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg'
       })
     })
-  });
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
+
+const position = new VectorSource();
+const vector = new VectorLayer({
+  source: position
+});
+
+var searchResult = new VectorLayer({
+  zIndex: 1
+});
+map.addLayer(searchResult);
+
+new AutoComplete({
+  selector: 'input[name="q"]',
+  source: function(term, response) {
+    var source = new VectorSource({
+      format: new GeoJSON(),
+      url: 'https://photon.komoot.de/api/?q=' + term
+    });
+    source.on('change', function() {
+      var texts = source.getFeatures().map(function(feature) {
+        var properties = feature.getProperties();
+        return (properties.city || properties.name || '') + ', ' +
+          (properties.street || '') + ' ' +
+          (properties.housenumber || '');
+      });
+      response(texts);
+      map.getView().fit(source.getExtent(), {
+        maxZoom: 19,
+        duration: 250
+      });
+    });
+    searchResult.setSource(source);
+  }
+});
+
+vector.setStyle(new Style({
+  image: new IconStyle({
+    src: './data/marker.png'
+  })
+}));
+map.addLayer(vector);
+
+//! [geolocation]
+navigator.geolocation.getCurrentPosition(function(pos) {
+  const coords = proj.fromLonLat([pos.coords.longitude, pos.coords.latitude]);
+  map.getView().animate({center: coords, zoom: 13});
+  position.addFeature(new Feature(new Point(coords)));
+});
+
+//! Pop-up bei Klick
+
+var overlay = new Overlay({
+  element: document.getElementById('popup-container'),
+  positioning: 'bottom-center',
+  offset: [0, -10]
+});
+
+map.addOverlay(overlay);
+
+map.on('click', function(e) {
+  overlay.setPosition();
+  var features = map.getFeaturesAtPixel(e.pixel);
+  if (features) {
+    overlay.getElement().innerHTML = 'Meine Position';
+    overlay.setPosition(e.coordinate);
+  }
 });
