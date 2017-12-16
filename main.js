9bc515fa66b34a2a1fde59b2a41eafdd1738c99b
pk <33088686+paulkae@users.noreply.github.com>
2017-12-16 15:39:10 +0100

    update

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-import "ol/ol.css";
-import "javascript-autocomplete/auto-complete.css";
-import proj from "ol/proj";
-import GeoJSON from "ol/format/geojson";
-import VectorLayer from "ol/layer/vector";
-import VectorSource from "ol/source/vector";
-import { apply } from "ol-mapbox-style";
-import AutoComplete from "javascript-autocomplete";
+import 'ol/ol.css';
+import proj from 'ol/proj';
+import GeoJSON from 'ol/format/geojson';
+import VectorLayer from 'ol/layer/vector';
+import VectorSource from 'ol/source/vector';
+import {
+  apply
+} from 'ol-mapbox-style';
+import Overlay from 'ol/overlay';
+import coordinate from 'ol/coordinate';
 
 var map = apply(
-  "map",
-  "data/style.json"
+  'map',
+  'data/style.json'
 );
 
 function fit() {
@@ -18,61 +20,19 @@ function fit() {
     duration: 250
   });
 }
-
-var selected;
-function getAddress(feature) {
-  var properties = feature.getProperties();
-  return (
-    (properties.city || properties.name || "") +
-    " " +
-    (properties.street || "") +
-    " " +
-    (properties.housenumber || "")
-  );
-}
-
-var searchResult = new VectorLayer({
-  zIndex: 9999
+var overlay = new Overlay({
+  element: document.getElementById('popup-container'),
+  positioning: 'bottom-center',
+  offset: [0, -10]
 });
-map.addLayer(searchResult);
-
-var onload, source;
-new AutoComplete({
-  selector: 'input[name="q"]',
-  source: function(term, response) {
-    if (onload) {
-      source.un("change", onload);
-    }
-    searchResult.setSource(null);
-    source = new VectorSource({
-      format: new GeoJSON(),
-      url: "https://photon.komoot.de/api/?q=" + term
-    });
-    onload = function(e) {
-      var texts = source.getFeatures().map(function(feature) {
-        return getAddress(feature);
-      });
-      response(texts);
-      fit();
-    };
-    source.once("change", onload);
-    searchResult.setSource(source);
-  },
-  onSelect: function(e, term, item) {
-    selected = item.getAttribute("data-val");
-    source.getFeatures().forEach(function(feature) {
-      if (getAddress(feature) !== selected) {
-        source.removeFeature(feature);
-      }
-    });
-    fit();
+map.addOverlay(overlay);
+map.on('click', function(e) {
+  overlay.setPosition();
+  var features = map.getFeaturesAtPixel(e.pixel);
+  if (features) {
+    var coords = features[0].getGeometry().getCoordinates();
+    var hdms = coordinate.toStringHDMS(proj.toLonLat(coords));
+    overlay.getElement().innerHTML = hdms;
+    overlay.setPosition(coords);
   }
 });
-map.on('singleclick', function(e) {
- var pos = proj.toLonLat(e.coordinate);
- window.location.href =
- 'https://student.ifip.tuwien.ac.at/geoweb/2017/g03/map/feedback.php?pos=' +
- pos.join(' ');
-});
-import sync from 'ol-hashed';
-sync(map);
